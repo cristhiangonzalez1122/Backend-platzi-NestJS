06d5fd6456ae3aa06018e60f11ed2af6bc6da781
cristhian gonzalez <cristhianmg1122@gmail.com>
2022-02-13 23:15:39 -0500

    e agrega entity

---
+++ src/users/entities/order.entity.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Product } from "src/products/entities/product.entity";
+import { User } from "./user.entity";
+
+
+export class Order {
+  date: Date;
+  user: User;
+  products: Product[];
+}
